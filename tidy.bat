592e0fc54d850d7e1b1ff3372e9c97051b47b87d
j <jdickinsp@gmail.com>
2023-09-23 12:15:08 -0400

    update

--- tidy.bat
+++ tidy.bat
@@ -1,4 +1,4 @@
 @set INCLUDES=-I./include -I../shared_libraries/stb
-@set SOURCES=./src/raytracer.c ./src/camera.c ./src/examples.c ./src/image.c ./src/loaders.c ./src/material.c ./src/matrix44.c ./src/raycast.c ./src/scene.c ./src/texture.c ./src/utils.c ./src/vectors.c 
+@set SOURCES=./src/*.c
 
 clang-tidy %SOURCES% -- %INCLUDES%
